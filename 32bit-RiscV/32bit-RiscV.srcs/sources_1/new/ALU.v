611b3de88b083e887f20774db75165e9c08cf68d
TONYHEGUAGUA <tonyhe_2015321@outlook.com>
2024-04-09 16:37:54 +0800

    complete ALU with error

--- 32bit-RiscV/32bit-RiscV.srcs/sources_1/new/ALU.v
+++ 32bit-RiscV/32bit-RiscV.srcs/sources_1/new/ALU.v
@@ -1,47 +1,33 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/27/2024 11:36:24 AM
-// Design Name: 
-// Module Name: ALU
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module ALU(
     input clock,
-    input [31:0]A,B,
-    input [3:0]ALU_op,
-    output [31:0]result,
-    output zero, less  // 添加逗号分隔符
+    input [31:0] A,
+    input [31:0] B,
+    input [3:0] ALU_op,
+    output [31:0] result,
+    output zero,
+    output less
 );
     wire [31:0] result_temp;
     wire zero_temp;
     wire less_temp;
+    wire [3:0] ALU_op_part;  // 中间信号
     
-    always @(posedge clock) begin
-        if (ALU_op == 3'b0000)
-            ALU_ADD  ALU_ADD_inst(
-            A,
-            B,
-            result_temp,
-            zero_temp,
-            less_temp);
+    assign ALU_op_part = ALU_op[3:0];  // 将部分信号赋值给中间信号
+    
+    always @(posedge) begin
+        if (ALU_op[2:0] == 3'b000)
+            ALU_Adder ALU_Adder_inst(A, B, ALU_op_part, result_temp, zero_temp, less_temp);
+        if ((ALU_op[2:0] == 3'b001) || (ALU_op[2:0] == 3'b101))
+            ALU_Shifter ALU_Shifter_inst(A, B, ALU_op_part, result_temp, zero_temp, less_temp);
+        if ((ALU_op[2:0] == 3'b100) || (ALU_op[2:0] == 3'b110) || (ALU_op[2:0] == 3'b111))
+            ALU_Logic ALU_Logic_inst(A, B, ALU_op_part, result_temp, zero_temp, less_temp);
+        if (ALU_op[2:0] == 3'b010)
+            ALU_Sub ALU_Sub_inst(A, B, ALU_op_part, result_temp, zero_temp, less_temp);
+        if (ALU_op[2:0] == 3'b011)
+            result_temp = B[31:0];
     end
     
-    assign result = result_temp;  // 将result_temp赋给result输出端口
+    assign result = result_temp;
     assign zero = zero_temp;
     assign less = less_temp;
     
